ae8cf976b35c9e414034ca60ee96fbd35c3204d4
Ali RaZa <aliraza.1121@yahoo.com>
2025-07-13 00:49:24 +0400

    feat(api): add rate limiting functionality with ApiRateLimitSubscriber and configure lock and rate limiter services

--- src/Controller/Api/V1/HandsetController.php
+++ src/Controller/Api/V1/HandsetController.php
@@ -9,6 +9,7 @@ use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
 
+
 class HandsetController extends AbstractController
 {
     public function __construct(
---
+++ src/EventSubscriber/ApiRateLimitSubscriber.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace App\EventSubscriber;
+
+use Symfony\Component\EventDispatcher\EventSubscriberInterface;
+use Symfony\Component\HttpKernel\Event\RequestEvent;
+use Symfony\Component\HttpKernel\Exception\TooManyRequestsHttpException;
+use Symfony\Component\RateLimiter\RateLimiterFactory;
+
+class ApiRateLimitSubscriber implements EventSubscriberInterface
+{
+    private RateLimiterFactory $apiLimiter;
+
+    public function __construct(RateLimiterFactory $apiLimiter)
+    {
+        $this->apiLimiter = $apiLimiter;
+    }
+
+    public function onKernelRequest(RequestEvent $event)
+    {
+        $request = $event->getRequest();
+        if (strpos($request->getPathInfo(), '/api/') !== 0) {
+            return;
+        }
+
+        $limiter = $this->apiLimiter->create($request->getClientIp());
+        $limit = $limiter->consume();
+
+        if (!$limit->isAccepted()) {
+            throw new TooManyRequestsHttpException(
+                $limit->getRetryAfter()->getTimestamp() - time(),
+                'Too many requests, please slow down.'
+            );
+        }
+    }
+
+    public static function getSubscribedEvents()
+    {
+        return [
+            'kernel.request' => ['onKernelRequest', 15],
+        ];
+    }
+}
